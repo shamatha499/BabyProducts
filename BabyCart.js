f3b06d06f820b838d4da0fc62d063a17c4206318
shamatha499 <shamanthakamanil399@gmail.com>
2024-12-09 11:41:34 +0530

    Add files via upload

---
+++ BabyCart.js
@@ -0,0 +1,13 @@
+function Box(){
+    var v =document.getElementById("a");
+    var value=v.value;
+    if ( value=="FirstCry")
+    {
+        window.location="https://www.firstcry.com";
+    }
+    else{
+        alert("Enter the search");
+    }
+         
+      
+}
